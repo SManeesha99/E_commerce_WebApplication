238279e15f2b7b4ddce6c34d8234f10c79865d9e
SManeesha99 <it20649012@my.sliit.lk>
2023-10-03 18:53:44 +0530

    update cart

--- cart.php
+++ cart.php
@@ -1,6 +1,9 @@
 <?php
-//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
-if(session_id() == '' || !isset($_SESSION)){session_start();}
+// Check if session is not already started, then start it.
+if (session_id() == '' || !isset($_SESSION)) {
+  session_start();
+}
+
 include 'config.php';
 ?>
 
