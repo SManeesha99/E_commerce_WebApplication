07e19a4800fb2b12eb9c56a1188c22820249a9a6
SManeesha99 <it20649012@my.sliit.lk>
2023-10-04 17:50:24 +0530

    changes

--- admin-update.php
+++ admin-update.php
@@ -1,57 +1,45 @@
 <?php
-// Check if session is not already started, then start it
-if (session_id() == '' || !isset($_SESSION)) {
-  session_start();
-}
 
-// Check if $_SESSION["type"] is set and not empty
-if (isset($_SESSION["type"]) && $_SESSION["type"] !== "") {
-  // The session variable "type" is set and not empty
-} else {
-  header("location:index.php");
-  exit(); // Add an exit to prevent further execution
-}
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
 
-if ($_SESSION["type"] != "admin") {
+if($_SESSION["type"]!="admin") {
   header("location:index.php");
-  exit(); // Add an exit to prevent further execution
 }
 
 include 'config.php';
 
 $_SESSION["products_id"] = array();
-if (isset($_REQUEST['quantity'])) {
-  $_SESSION["products_id"] = $_REQUEST['quantity'];
-}
+$_SESSION["products_id"] = $_REQUEST['quantity'];
+
 
 $result = $mysqli->query("SELECT * FROM products ORDER BY id asc");
-$i = 0;
-$x = 1;
+$i=0;
+$x=1;
 
-if ($result) {
-  while ($obj = $result->fetch_object()) {
-    if (empty($_SESSION["products_id"][$i])) {
+if($result) {
+  while($obj = $result->fetch_object()) {
+    if(empty($_SESSION["products_id"][$i])) {
       $i++;
       $x++;
-    } else {
-      $newqty = $obj->qty + intval($_SESSION["products_id"][$i]); // Use intval to sanitize input as an integer
-      if ($newqty < 0) $newqty = 0; // So, Qty will not be in negative.
-
-      // Use prepared statement to update the product quantity
-      $stmt = $mysqli->prepare("UPDATE products SET qty = ? WHERE id = ?");
-      $stmt->bind_param("ii", $newqty, $x);
-      
-      if ($stmt->execute()) {
+    }
+    else {
+      $newqty = $obj->qty + $_SESSION["products_id"][$i];
+      if($newqty < 0) $newqty = 0; //So, Qty will not be in negative.
+      $update = $mysqli->query("UPDATE products SET qty =".$newqty." WHERE id =".$x);
+      if($update)
         echo 'Data Updated';
-      }
 
-      $stmt->close();
       $i++;
       $x++;
     }
   }
 }
 
-header("location:success.php");
-exit(); // Add an exit to prevent further execution
+
+
+header ("location:success.php");
+
+
+
 ?>
--- cart.php
+++ cart.php
@@ -1,9 +1,6 @@
 <?php
-// Check if session is not already started, then start it.
-if (session_id() == '' || !isset($_SESSION)) {
-  session_start();
-}
-
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
 include 'config.php';
 ?>
 
